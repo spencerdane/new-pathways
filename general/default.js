d203cf6124ffcb20c9560c6c284f0a7b29927475
spazbot17 <spencerthedane@gmail.com>
2024-01-28 17:47:08 +0530

    Underlay added when mobile nav menu is open.

--- general/default.js
+++ general/default.js
@@ -1,14 +1,17 @@
 $(window).on("load resize", function (){
     adjustNav();
+    $(".mobile-underlay").removeClass("underlay");
 })
 
 $(".menu-icon").on("click", function() {
     $("nav").toggleClass("hidden");
+    $(".mobile-underlay").toggleClass("underlay");
 })
 
-$("main").on("click", function () {
+$(".mobile-underlay").on("click", function () {
     if (!$("nav").hasClass(".hidden")) {
         $("nav").addClass("hidden");
+        $(".mobile-underlay").removeClass("underlay");
     }
 })
 
