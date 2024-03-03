37fb6d70b784631238641e0998c0497e24c435fc
spazbot17 <spencerthedane@gmail.com>
2024-03-03 12:28:55 +0530

    Create local server.

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
-var allHidden = true;
-var lastAnswer = "";
+import express from "express";
+import bodyParser from "body-parser";
 
+const port = 3000;
+const app = express();
 
-$(".btn.question").on("click", function (){
-    var answer = $(this).attr("class");
-    answer = getAnswer(answer);
-    revealAnswer(answer);
-    lastAnswer = answer;
-})
+app.use(bodyParser.urlencoded({extended: true}));
 
-function revealAnswer(answer, last = lastAnswer) {
-    if ($(".answer.block" + answer).hasClass("hidden") && allHidden){
-        $(".answer.block" + answer).slideDown(500);
-        $("html").animate({scrollTop: $(answer).position().top}, 1000);
-        $(".answer.block" + answer).removeClass("hidden");
-        allHidden = false;
-    } else if (!$(".answer.block" + answer).hasClass("hidden")) {
-        $(".answer.block" + answer).slideUp(500);
-        $(".answer.block" + answer).addClass("hidden");
-        allHidden = true;
-    } else if (allHidden === false) {
-        $(".answer.block" + lastAnswer).slideUp(500);
-        $(".answer.block" + lastAnswer).addClass("hidden");
-        $(".answer.block" + answer).slideDown(500);
-        $(".answer.block" + answer).removeClass("hidden");
-        setTimeout(function () {
-            $("html").animate({scrollTop: $(answer).position().top}, 1000);
-        }, 500);
-    }
-}
-
-function getAnswer(question) {
-    if (question.includes("cbt")) {
-        return ".cbt";
-    } else if (question.includes("help-how")) {
-        return ".help-how";
-    } else if (question.includes("start")) {
-        return ".start";
-    } else {
-        console.log("Button not recognized.");
-    }
-}
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});     
---
+++ views/index.js
@@ -0,0 +1,43 @@
+var allHidden = true;
+var lastAnswer = "";
+
+
+$(".btn.question").on("click", function (){
+    var answer = $(this).attr("class");
+    answer = getAnswer(answer);
+    revealAnswer(answer);
+    lastAnswer = answer;
+})
+
+function revealAnswer(answer, last = lastAnswer) {
+    if ($(".answer.block" + answer).hasClass("hidden") && allHidden){
+        $(".answer.block" + answer).slideDown(500);
+        $("html").animate({scrollTop: $(answer).position().top}, 1000);
+        $(".answer.block" + answer).removeClass("hidden");
+        allHidden = false;
+    } else if (!$(".answer.block" + answer).hasClass("hidden")) {
+        $(".answer.block" + answer).slideUp(500);
+        $(".answer.block" + answer).addClass("hidden");
+        allHidden = true;
+    } else if (allHidden === false) {
+        $(".answer.block" + lastAnswer).slideUp(500);
+        $(".answer.block" + lastAnswer).addClass("hidden");
+        $(".answer.block" + answer).slideDown(500);
+        $(".answer.block" + answer).removeClass("hidden");
+        setTimeout(function () {
+            $("html").animate({scrollTop: $(answer).position().top}, 1000);
+        }, 500);
+    }
+}
+
+function getAnswer(question) {
+    if (question.includes("cbt")) {
+        return ".cbt";
+    } else if (question.includes("help-how")) {
+        return ".help-how";
+    } else if (question.includes("start")) {
+        return ".start";
+    } else {
+        console.log("Button not recognized.");
+    }
+}
