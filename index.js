7e8a14b22fe7c332a615b1d2402542e7f0ecfe44
spazbot17 <spencerthedane@gmail.com>
2024-03-03 13:22:28 +0530

    Add styles and scripts to all pages.

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const port = 3000;
 const app = express();
 
 const styleAndScript = {
-    about: {style: "about", script: "about"},
-    contact: {style: "contact", script: "contact"},
+    about: {style: "about", script: ""},
+    contact: {style: "contact", script: ""},
     course: {style: "course", script: "course"},
     faqs: {style: "faqs", script: "faqs"},
     index: {style: "index", script: "index"},
-    origins: {style: "origins", script: "origins"},
-    practitioner: {style: "practitioner", script: "practitioner"}
+    origins: {style: "origins", script: ""},
+    practitioner: {style: "practitioner", script: ""}
 }
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,6 +19,30 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs", styleAndScript["index"]);
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs", styleAndScript["about"]);
+});
+
+app.get("/practitioner", (req, res) => {
+    res.render("practitioner.ejs", styleAndScript["practitioner"]);
+});
+
+app.get("/origins", (req, res) => {
+    res.render("origins.ejs", styleAndScript["origins"]);
+});
+
+app.get("/course", (req, res) => {
+    res.render("course.ejs", styleAndScript["course"]);
+});
+
+app.get("/faqs", (req, res) => {
+    res.render("faqs.ejs", styleAndScript["faqs"]);
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs", styleAndScript["contact"]);
 });
 
 app.listen(port, () => {
